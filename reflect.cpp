5f535c9a0573dd548286ffc805ed228f167e08bd
陈安卓 <chenanzhuo517@gmail.com>
2018-07-27 09:55:32 +0800

    v1.0.2
    1:rename register_class to register_type

--- reflect.cpp
+++ reflect.cpp
@@ -1,14 +1,27 @@
 
 #include <iostream>
+#include <string>
 
 #include "reflect.h"
 #include "test.h"
 
+constexpr char class_[][10] = { "test", "test1", "test2" };
+
+template <int N>
+void print()
+{
+	auto t = get_register_type(class_[N - 1]);
+	t.print();
+	print<N - 1>();
+}
+
+template <> void print<0>() {}
+
 int main()
 {
-	auto t = get_class("test");
-	t.print();
-	
+	print<3>();
+	auto n1 = get_register_type("int");
+
 	return 0;
 }
 
--- test.h
+++ test.h
@@ -7,13 +7,33 @@
 #include "reflect.h"
 
 class test;
-register_class(test);
+register_type(test);
 class test
 {
 public:
 	test() {}
 	virtual ~test() {}
 	void print() { std::cout << "test" << std::endl; }
+};
+
+class test1;
+register_type(test1);
+class test1
+{
+public:
+	test1() {}
+	virtual ~test1() {}
+	void print() { std::cout << "test1" << std::endl; }
+};
+
+class test2;
+register_type(test2);
+class test2
+{
+public:
+	test2() {}
+	virtual ~test2() {}
+	void print() { std::cout << "test2" << std::endl; }
 };
 
 #endif // !__H_TEST_H__
